edbc8985b5e08ceef9d9808e3870408daa1a7378
mosesWigglesworth <moseswigglesworth@gmail.com>
2025-01-26 14:06:48 +0530

    Eureka Server App Dev Commit

---
+++ eureka-server-ms/src/main/java/com/stc/eureka/server/EurekaServerMsApplication.java
@@ -0,0 +1,20 @@
+package com.stc.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wigglesworth.m
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@RefreshScope
+public class EurekaServerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerMsApplication.class, args);
+	}
+
+}
